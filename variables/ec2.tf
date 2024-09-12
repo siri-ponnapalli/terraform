2d9744edae1aa8a97d59a84a29dc9d6a0ef46dbd
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-13 01:33:48 +0530

    added tags

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -3,9 +3,7 @@ resource "aws_instance" "terraform" {
    ami = var.ami_id
    instance_type = var.instance_type
    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-   tags  = {
-      Name = ""
- }
+   tags  = var.tags
 
 }
 
--- variables/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-instance_type = "t3.medium"
-instance_count = 1
-availability_zones = ["us-east-1a"]
