113bc0ad985a6784e6405699591d1761dc974138
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-13 00:33:30 +0530

    tf changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -4,7 +4,9 @@ resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    tags  = var.tags
+    tags = {
+        Name = "terraform"
+    }
            
  }
 
